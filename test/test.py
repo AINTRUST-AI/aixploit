b9932fa0cde2d4e89fa3da85969b7aec609ab165
AINTrust <contact@aintrust.ai>
2024-12-01 00:02:04 +0100

    clean up

--- test/test.py
+++ test/test.py
@@ -1,17 +1,10 @@
-import sys, os
+import os
 from datetime import datetime
-
-sys.path.append(
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-)  # Automatically detect the current folder # Adjust this path as necessary
 from aixploit.plugins import PromptInjection
 from aixploit.core import run
 
 
-target1 = ["Ollama", "http://localhost:11434/v1", "mistral"]
-target2 = ["Openai", "", "gpt-3.5-turbo"]
-
-
+target = ["Openai", "", "gpt-3.5-turbo"]
 attackers = [
     PromptInjection("quick"),
     # PromptInjection("full")
@@ -20,14 +13,13 @@ attackers = [
 start_time = datetime.now()
 print("Redteaming exercise started at : ", start_time.strftime("%H:%M:%S"))
 
-# conversation, attack_prompts_malicious, success_rates_percentage, total_tokens, total_cost = run(attackers, target1, os.getenv("OLLAMA_API_KEY"))
 (
     conversation,
     attack_prompts,
     success_rates_percentage,
     total_tokens,
     total_cost,
-) = run(attackers, target2, os.getenv("OPENAI_KEY"))
+) = run(attackers, target, os.getenv("OPENAI_KEY"))
 
 for idx, attacker in enumerate(attackers):  # {{ edit_1 }}
     try:
